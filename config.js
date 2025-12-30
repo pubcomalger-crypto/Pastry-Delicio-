b0356bc831495ba026cf6ccac67bcb69426253dc
pubcomalger-crypto <pubcom.alger@gmail.com>
2025-12-30 12:10:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ const profileConfig = {
         facebook: "https://www.facebook.com/share/1CED1f2N78/",
         instagram: "https://www.instagram.com/delicio_pastry?igsh=MTQ0NXB5YTFrZ2Fkbw==",
         tiktok: "https://www.tiktok.com/@delicio.pastry?_r=1&_t=ZS-92cocxrRJo8",
-        whatsapp: "0778051351",
+        whatsapp: "+213778051351",
         telegram: "+213 562 33 14 08",
         linkedin: "daisyparkhouse",
         snapchat: "https://www.snapchat.com/@sheikh_tidiane?sender_web_id=792b2a4d-05d3-4e8f-a5e0-1aeb89eba722&device_type=desktop&is_copy_url=true",
@@ -83,4 +83,5 @@ if (typeof module !== 'undefined' && module.exports) {
 } else {
     window.profileConfig = profileConfig;
 }
+
 
